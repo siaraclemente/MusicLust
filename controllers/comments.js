c5a6b49d35a82a590fa737041b1459dd9060ba93
Siara <siara.clemente@gmail.com>
2019-02-28 16:01:42 -0600

    reworked function so you can only comment if logged in, everything works locally, trying to get this set up so it works when deployed on heroku

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,11 +6,16 @@ module.exports = {
 };
 
 function addComment(req, res) {
-    console.log('HELLO');
-    Perform.findById(req.params.id,function(err, perform) {
-    perform.comments.push(req.body);
-    perform.save(function(err) {
-      res.redirect('/performs');
+    if(req.user){
+      // From here you can control commenting - if a user is logged in, make the comment
+      Perform.findById(req.params.id,function(err, perform) {
+      perform.comments.push(req.body);
+      perform.save(function(err) {
+        res.redirect('/performs');
+      });
     });
-  });
+    } else {
+      // otherwise we can send them somewhere else
+      res.redirect('Send them somewhere else')
+    }
 }
